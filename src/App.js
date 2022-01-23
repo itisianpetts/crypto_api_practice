7d686eff776f094d1bb0ec020ccc36d385a86f35
itisianpetts <itisianpetts@gmail.com>
2022-01-23 17:27:27 +0000

    Add pages, routing and basic antd styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,59 @@
 import React from 'react';
+import './App.css';
+import { Routes, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+import {
+  Navbar,
+  Homepage,
+  Exchanges,
+  News,
+  Cryptocurrencies,
+  CryptoDetails,
+} from './components';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route exact path="/" element={<Homepage />}></Route>
+              <Route exact path="/exchanges" element={<Exchanges />}></Route>
+              <Route
+                exact
+                path="/cryptocurrencies"
+                element={<Cryptocurrencies />}
+              ></Route>
+              <Route
+                exact
+                path="/crypto/:coinId"
+                element={<CryptoDetails />}
+              ></Route>
+              <Route exact path="/news" element={<News />}></Route>
+            </Routes>
+          </div>
+        </Layout>
+        <div className="footer">
+          <Typography.Title
+            level={5}
+            style={{ color: 'white', textAlgin: 'center' }}
+          >
+            CryptoAPI <br />
+            All rights reserved.
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
